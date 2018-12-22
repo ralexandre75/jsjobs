f2013cc74b8c6b7fdbd0df7ed21ecc4eeb000c12
ralexandre75 <rene.alexandre@grimoire-design.fr>
2018-12-22 23:20:24 +0100

    generate radio buttons

--- src/app/job-add-form/job-add-form.component.ts
+++ src/app/job-add-form/job-add-form.component.ts
@@ -18,6 +18,31 @@ export class JobAddFormComponent implements OnInit {
     {id:5, name: "indépendant", value: "freelance"}
   ];
 
+  currencies = [
+    {id:1, name:"euros", value:"EU", symbol:"€"}, 
+    {id:2, name:"livre sterling", value:'POUNDS', symbol:"£"},
+    {id:3, name:"franc CFA", value:"CFA", symbol:"CFA"}, 
+    {id:4, name:"dollars", value:"CAD", symbol:"$"}
+  ];
+  
+  statuses = [
+    {id:1, name:"cadre", value:"executive"}, 
+    {id:2, name:"employée", value:"employee"}
+  ];
+
+  experiences = [
+    {id:1, name:"junior", value:"junior"}, 
+    {id:2, name:"medior", value:"medior"},
+    {id:3, name:"senior", value:"senior"}
+  ];
+
+  areas = [
+    {id:1, name:"aucun déplacement", value:"none"}, 
+    {id:2, name:"déplacement régionaux", value:"region"},
+    {id:3, name:"déplacement nationaux", value:"nation"},
+    {id:4, name:"déplacement internationaux", value:"international"}
+  ];
+
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -29,8 +54,8 @@ export class JobAddFormComponent implements OnInit {
       zipcode: 35,
       description: '',
       contract: '',
-      salary: 0,
-      currency: 'euros',
+      salary: null,
+      currency: '',
       startdate: new Date(),
       experience: '',
       status: '',
