c4625c304fe29f7c3df8ae3bd42fe4d6c206176e
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-01-07 10:39:20 +0100

    Make Brother Components Communicate Using a Subject RxJS

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -2,17 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
 
+  jobs = [];
+  jobsSubject = new Subject();
 
   constructor(private _httpClient: HttpClient) { }
 
   getJobs() {
     return this._httpClient.get<any>('data/jobs.json')
       .pipe(tap(x => console.log(x), map(res => res)));
+  }
+
+  addJob(jobData){
+    jobData.id = Date.now();
+    return this.jobsSubject.next(jobData);
   }
 }
