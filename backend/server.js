e2e16c889d540eec33e24352d477e94b3a0859ed
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-01-10 10:25:33 +0100

    send an array of object from our API REST

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+let data = require('./jobs')
 
 app.use(bodyParser.json());
 
 const api = express.Router();
 
 api.get('/jobs', (req, res) => {
-    res.json({ success: true, message: 'Hello word'});
+    // res.json({ success: true, message: 'hello world'});
+    res.json(data.jobs);
 });
 
 app.use('/api', api);
