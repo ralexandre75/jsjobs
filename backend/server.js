4b7691d91efb6e1b0f4e64f80ef2333f249c4b82
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-01-10 09:48:00 +0100

    create an API REST with express.js

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+const api = express.Router();
+
+api.get('/jobs', (req, res) => {
+    res.json({ success: true, message: 'Hello word'});
+});
+
+app.use('/api', api);
+
+const port = 4201;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+});
